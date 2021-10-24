0c8ea8f803650af94d23482a9682349f83c7deb7
eldelahoz <andelahoz13@gmail.com>
2021-10-24 08:44:52 -0500

    Add html test, and use template links with inheritance

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,6 +1,7 @@
 # from django.test import TestCase
 from config.wsgi import *
-from core.erp.models import Type
+from core.erp.models import Category
+# from core.erp.models import Type
 
 # Create your tests here.
 
@@ -8,14 +9,19 @@ from core.erp.models import Type
 # Type(name='Viba').save()
 # Type(name='Montana').save()
 
-print(Type.objects.all())
+# print(Type.objects.all())
 
 # f = Type.objects.filter(name__icontains='adm')
 # f = Type.objects.filter(name__startswith='A')
 # f = Type.objects.filter(name__in=['Presidente', 'Administrador']).count()
 # f = Type.objects.filter(name__icontains='Presidente').query
-f = Type.objects.filter(name__endswith='a').exclude(id=5)
-print(f)
+# f = Type.objects.filter(name__endswith='a').exclude(id=5)
+# print(f)
 
-for i in Type.objects.filter(name__endswith='a')[2:]:
-    print(i.name)
+# for i in Type.objects.filter(name__endswith='a')[2:]:
+#    print(i.name)
+print(Category.objects.all())
+if Category.objects.filter(name__in=['Bebidas']):
+    print("Si existe")
+else:
+    print("No existe")
--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from core.erp.models import Category, Product
 # Create your views here.
+
+
+def myfirstview(request):
+    data = {
+        'name' : 'DelaHoz',
+        'categorias': Category.objects.all()
+    }
+    return render(request, 'index.html', data) # JsonResponse(data) HttpResponse("Bonjour les amis")
+
+
+def mysecondview(request):
+    data = {
+        'name' : 'DelaHoz',
+        'categorias': Category.objects.all(),
+        'products': Product.objects.all()
+    }
+    return render(request, 'second.html', data) # JsonResponse(data) HttpResponse("Bonjour les amis")
