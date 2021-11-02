436de8a8e8a5daccd3a08b70773b00d22d7828ed
Andres De la Hoz <andelahoz13@gmail.com>
2021-11-02 15:27:09 -0500

    FIx error verification creation category and add jquery-alert

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,9 +1,32 @@
-function alert_error(obj){
+function alert_error(obj) {
     var html = '';
-    $.each(obj, function (key, value){
+    $.each(obj, function (key, value) {
         html += '<li>' + key + ': ' + value + '</li>';
 
     });
 //    console.log(html);
-    return(html);
+    return (html);
+}
+
+function alert_jqueryconfirm() {
+    $.confirm({
+        title: 'Confirm!',
+        content: 'Simple confirm!',
+        buttons: {
+            confirm: function () {
+                $.alert('Confirmed!');
+            },
+            cancel: function () {
+                $.alert('Canceled!');
+            },
+            somethingElse: {
+                text: 'Something else',
+                btnClass: 'btn-blue',
+                keys: ['enter', 'shift'],
+                action: function () {
+                    $.alert('Something else?');
+                }
+            }
+        }
+    });
 }
