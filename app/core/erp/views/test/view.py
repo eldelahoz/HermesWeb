71456923e0a09dacd3fa61bef89cd4da9f4113a8
eldelahoz <andelahoz13@gmail.com>
2022-02-10 15:15:55 +0000

    Add tests view for select with two moduls

---
+++ app/core/erp/views/test/view.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
+from core.erp.forms import TestForm
+from core.erp.models import Product
+
+
+class TestView(TemplateView):
+    template_name = 'tests.html'
+
+    @method_decorator(csrf_exempt)
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'search_product_id':
+                for i in Product.objects.filter(cat_id=request.POST['id']):
+                    data.update({i.id: i.nombre})
+                print(data)
+            else:
+                data['error'] = 'No ha ingresado a ninguna opcion'
+                # data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Select Anidados - Django'
+        context['form'] = TestForm()
+        return context
